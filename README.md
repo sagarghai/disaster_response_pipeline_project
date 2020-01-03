57de49ad57ff99a7f9e9c33fb0c6d639ce3a46a3
Sagar Ghai <sagarghai18695@gmail.com>
2020-01-04 00:28:50 +0530

    created process_data file and updated readme fully

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
 # Disaster Response Pipeline Project
 
+## Introduction:
+This is the project for classifying the disaster relief messages that are being sent to a disaster relief agency. Broadly, there are three steps involved in the complete project pipeline:
+- ETL Pipeline
+- ML Pipeline
+- Flask Web App
+
+### ETL Pipeline:
+In a python script, `process_data.py`, there are the following steps involved:
+- Load messages and categories data from `.csv` files in a dataframe.
+- Merge two dataframes in a single dataframe.
+- Split single `categories` column into multiple columns for MultiClass classification.
+- Store the dataframe in a sqlite database.
+
+### ML Pipeline:
+In a python script, `train_classifier.py`, there are the following steps involved:
+- Load data from sqlite database.
+- Split data into features and target variables.
+- Lemmatize and tokenize textual data using NLTK.
+- Build a ML pipeline using `CountVectorizer`, `TfidfTransformer` and `AdaBoostClassifier`.
+- Tune the model using GridSearch.
+- Print the classification report.
+- Save the classification model to a pickle file.
+
+### Flask Web App:
+In the python script, `run.py`, there are the following steps:
+- Load the model from pickle file.
+- Create visualizations for different categorizations:
+    - Distribution of Message Genres
+    ![](images/message_genre.png)
+    - Distribution of Categories
+    ![](images/category_distribution.png)
+    - Word Frequency
+    ![](images/word_frequency.png)
+
 ### Instructions:
 1. Run the following commands in the project's root directory to set up your database and model.
 
--- data/process_data.py
+++ data/process_data.py
@@ -1,22 +1,98 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+def load_data(messages_path, categories_path):
+    """
+    This function loads data from the provided filepaths
+    onto two dataframes and then merges them into one.
+    Args:
+      messages_path: messages.csv file path
+      categories_path: categories.csv file path
+    Returns:
+      df: merged dataframe containing messages and categories
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_path)
+    # load categories dataset
+    categories = pd.read_csv(categories_path)
+
+    # merge messages and categories in a single dataframe
+    df = pd.merge(messages, categories, left_on='id', right_on='id')
+
+    return df
+
+
+def process_categories(df):
+    """
+    Processes the dataframe to create categories, change them
+    into integer values and rename category columns
+    Args:
+      df: Dataframe containgin messages and categories
+    Returns:
+      categories: categories dataframe
+    """
+    # split categories column and create categories dataframe
+    categories = df.categories.str.split(';', expand=True)
+
+    # select first row of the dataframe
+    row = categories.iloc[0]
+
+    # split the values and get the string until second last character
+    def category_colnames(x):
+        return [str(y)[:-2] for y in x]
+    # assign categories dataframe column names
+    categories.columns = category_colnames(row)
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(
+            lambda x: x[-1] if int(x[-1]) < 2 else 1)
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(str).astype(int)
+
+    return categories
 
 
 def clean_data(df):
-    pass
+    """
+    Clean the dataset
+    Args:
+      df: merged dataframe
+    Returns:
+      df: cleaned dataframe
+    """
+    # get categories in a seperate dataframe
+    categories = process_categories(df)
+    # remove categories column
+    df = df.drop(['categories'], axis=1)
+    # concat merged dataframe (without categories column) and categories
+    # dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # remove duplicate rows
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    save dataframe to sqlite db
+    Args:
+      df: merged dataframe
+      database_filename: name of database file
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False)
 
 
 def main():
     if len(sys.argv) == 4:
 
-        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        messages_filepath, categories_filepath, database_filepath = sys.argv[
+            1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
@@ -24,18 +100,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
